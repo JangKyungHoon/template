253d2999107d4e90ffd6838d44ed23b2ca2e764c
kyunghoon.jang <kyunghoon.jang@navercorp.com>
2019-08-10 10:57:47 +0900

    gitignore 추가 인터셉터 수정 log4j maven 추가

--- src/main/java/com/template/app/common/interceptor/CommonInterceptor.java
+++ src/main/java/com/template/app/common/interceptor/CommonInterceptor.java
@@ -1,31 +1,31 @@
 package com.template.app.common.interceptor;
 
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-public class LoginInterceptor implements HandlerInterceptor {
-	Logger logger = Logger.getLogger(LoginInterceptor.class);
+public class CommonInterceptor extends HandlerInterceptorAdapter {
+	Logger logger = Logger.getLogger(CommonInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		logger.info("call preHandle");
-		return HandlerInterceptor.super.preHandle(request, response, handler);
+		return super.preHandle(request, response, handler);
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		logger.info("call postHandle");
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		super.postHandle(request, response, handler, modelAndView);
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 		logger.info("call afterCompletion");
-		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+		super.afterCompletion(request, response, handler, ex);
 	}
 	
 }
